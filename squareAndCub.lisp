d278aaf25f16d4178c571e0d9392a11ed2c79289
Jhon <jjbarrei@espol.edu.ec>
2015-01-07 23:45:49 -0500

    Scripts de ciertos programas

---
+++ squareAndCub.lisp
@@ -0,0 +1,19 @@
+(loop for i from 1 to 10 do
+	(princ "The square of " )
+	(princ i )
+	(princ " is ")
+	(princ (* i i))
+	(princ ".")
+	(princ "
+")
+		(if (or (= i 7) (= i 5) )
+			(and 
+				(princ " THE CUBE OF ")
+				(princ i )
+				(princ " IS ")
+				(princ (* i i i) )
+				(princ ".")
+				(princ "
+")
+				))
+	)
